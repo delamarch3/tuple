50f742159bd81311162efbd0404c4a5866273a8d
Subhan <68732277+delamarch3@users.noreply.github.com>
2025-05-03 21:03:18 +0100

    add benchmarks

--- benches/evaluate.rs
+++ benches/evaluate.rs
@@ -56,9 +56,24 @@ fn evaluate_benchmark(c: &mut Criterion) {
         })
     });
 
-    let expr = ident("brand").eq(lit("Brand#44"));
+    let expr = ident("partkey")
+        .add(lit(1).sub(lit(1).mul(lit(1).div(lit(1).add(lit(1))))))
+        .and(lit(1).sub(lit(1).mul(lit(1).div(lit(1).add(lit(1))))));
     let expr = PhysicalExpr::new(expr, &schema);
-    c.bench_function("select 1/25 (brand == Brand#44)", |b| {
+    c.bench_function(
+        "long expr (partkey + (1 - 1 * 1 / 1 + 1) and (1 - 1 * 1 / 1 + 1))",
+        |b| {
+            b.iter(|| {
+                black_box(tuples.iter().for_each(|tuple| {
+                    evaluate(&tuple, &expr);
+                }))
+            })
+        },
+    );
+
+    let expr = ident("partkey").between(lit(1), lit(100_000));
+    let expr = PhysicalExpr::new(expr, &schema);
+    c.bench_function("between (partkey between 1 and 100_000)", |b| {
         b.iter(|| {
             black_box(tuples.iter().for_each(|tuple| {
                 evaluate(&tuple, &expr);
@@ -66,15 +81,23 @@ fn evaluate_benchmark(c: &mut Criterion) {
         })
     });
 
-    let expr = ident("container").eq(lit("WRAP CAN"));
+    let expr = ident("mfgr").in_list(vec![
+        lit("Manufacturer#1"),
+        lit("Manufacturer#2"),
+        lit("Manufacturer#3"),
+        lit("Manufacturer#4"),
+    ]);
     let expr = PhysicalExpr::new(expr, &schema);
-    c.bench_function("select 1/40 (container == WRAP CAN)", |b| {
-        b.iter(|| {
-            black_box(tuples.iter().for_each(|tuple| {
-                evaluate(&tuple, &expr);
-            }))
-        })
-    });
+    c.bench_function(
+        "in list (mfgr in (Manufacturer#1, Manufacturer#2, Manufacturer#3, Manufacturer#4))",
+        |b| {
+            b.iter(|| {
+                black_box(tuples.iter().for_each(|tuple| {
+                    evaluate(&tuple, &expr);
+                }))
+            })
+        },
+    );
 
     let expr = contains(vec![
         concat(vec![ident("container"), ident("container")]),
